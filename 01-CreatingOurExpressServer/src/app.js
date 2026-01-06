c3e96cefa5a711a6ad4243d97c1ba400f14cf625
Shaikh Siddique <sheikhsiddique9630@gmail.com>
2026-01-06 07:36:00 -0800

    Creating our Express Server

--- 01-CreatingOurExpressServer/src/app.js
+++ 01-CreatingOurExpressServer/src/app.js
@@ -1,30 +1,19 @@
 const express = require("express");
+
 const app = express();
 
-app.use(
-    "/user",
-    (req, res, next) => {
-        console.log("Handling the route user! (1)");
-        next();
-    },
-    (req, res, next) => {
-        console.log("Handling the route user 2! (2)");
-        next();
-    },
-    (req, res, next) => {
-        console.log("Handling the route user 3! (3)");
-        next();
-    },
-    (req, res, next) => {
-        console.log("Handling the route user 4! (4)");
-        next();
-    },
-    (req, res) => {
-        console.log("Handling the route user 5! (final)");
-        res.send("5th Response!");
-    }
-);
+app.use('/', (req, res) => {
+    res.send("I Am Learning Backend");
+})
 
-app.listen(7777, () => {
-    console.log("server is running on port 7777");
+app.use('/test', (req, res) => {
+    res.send("Hello Form The Server");
+})
+
+app.use('/next', (req, res) => {
+    res.send("Hello World");
+})
+
+app.listen(5000, () => {
+    console.log("server is running on port 5000");
 });
